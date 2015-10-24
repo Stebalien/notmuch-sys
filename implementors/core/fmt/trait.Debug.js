bca5ed710c833a526a61d206947786fa852150b8
Steven Allen <steven@stebalien.com>
2015-10-24 16:35:26 -0400

    Update docs for 968db8dcc20a231b391e50b138a275601c68d2f7
    Add metadata

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['notmuch_sys'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='notmuch_sys/enum.notmuch_status_t.html' title='notmuch_sys::notmuch_status_t'>notmuch_status_t</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='notmuch_sys/enum.notmuch_sort_t.html' title='notmuch_sys::notmuch_sort_t'>notmuch_sort_t</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='notmuch_sys/enum.notmuch_exclude_t.html' title='notmuch_sys::notmuch_exclude_t'>notmuch_exclude_t</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='notmuch_sys/enum.notmuch_message_flag_t.html' title='notmuch_sys::notmuch_message_flag_t'>notmuch_message_flag_t</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='notmuch_sys/enum.notmuch_database_mode_t.html' title='notmuch_sys::notmuch_database_mode_t'>notmuch_database_mode_t</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
