53bae1ce8f3f8c7772b60a8362df6d1cb078e757
Steven Allen <steven@stebalien.com>
2015-10-24 16:39:03 -0400

    Add licensing text

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,22 @@
+/*
+ * Copyright © 2009 Carl Worth, 2015 Steven Allen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/ .
+ */
+
 //! Not much of an email library, just index and search.
+
 extern crate libc;
 use libc::{c_void, c_char, time_t, c_uint, c_double, c_ulong, c_int};
 
