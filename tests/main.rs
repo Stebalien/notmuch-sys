fabc7702d2f7e5c0db40cd4eee4f4f8ecadf3225
Steven Allen <steven@stebalien.com>
2016-01-18 23:53:22 -0500

    rustfmt tests

--- tests/main.rs
+++ tests/main.rs
@@ -17,45 +17,38 @@ Cool body!";
 
 #[test]
 fn it_works() {
-    let dir = tempdir::TempDir::new("notmuch")
-        .expect("failed to create temporary directory");
+    let dir = tempdir::TempDir::new("notmuch").expect("failed to create temporary directory");
 
     let msg_path = dir.path().join("cur/message");
 
-    fs::create_dir(msg_path.parent().unwrap())
-        .expect("failed to maildir directory");
+    fs::create_dir(msg_path.parent().unwrap()).expect("failed to maildir directory");
 
-    let mut msg_file = File::create(&msg_path)
-        .expect("failed to create message file");
+    let mut msg_file = File::create(&msg_path).expect("failed to create message file");
 
     msg_file.write_all(MESSAGE)
-        .expect("failed to write message");
+            .expect("failed to write message");
 
     unsafe {
         let mut db = ptr::null_mut();
         {
             let dir_c_str = CString::new(dir.path()
-                                         .as_os_str()
-                                         .as_bytes()).unwrap();
-            assert_eq!(
-                notmuch_database_create(dir_c_str.as_ptr(), &mut db),
-                notmuch_status_t::SUCCESS
-            );
+                                            .as_os_str()
+                                            .as_bytes())
+                                .unwrap();
+            assert_eq!(notmuch_database_create(dir_c_str.as_ptr(), &mut db),
+                       notmuch_status_t::SUCCESS);
         }
         let mut msg = ptr::null_mut();
         {
             let msg_c_str = CString::new(msg_path.as_os_str()
-                                         .as_bytes()).unwrap();
-            assert_eq!(
-                notmuch_database_add_message(db, msg_c_str.as_ptr(), &mut msg),
-                notmuch_status_t::SUCCESS
-            );
+                                                 .as_bytes())
+                                .unwrap();
+            assert_eq!(notmuch_database_add_message(db, msg_c_str.as_ptr(), &mut msg),
+                       notmuch_status_t::SUCCESS);
         }
         let header = notmuch_message_get_header(msg, b"From\0" as *const u8 as *const i8);
         assert!(!header.is_null());
-        assert_eq!(
-            CStr::from_ptr(header),
-            CStr::from_ptr(b"alice@example.com\0" as *const u8 as *const i8)
-        );
+        assert_eq!(CStr::from_ptr(header),
+                   CStr::from_ptr(b"alice@example.com\0" as *const u8 as *const i8));
     }
 }
